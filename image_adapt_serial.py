04ca2b552ea3c1a5bc9a63695c4c870c063416af
Christopher K. Lee <chris.kelvin.lee@gmail.com>
2011-11-28 11:39:04 -0500

    time() and imsave

--- image_adapt_serial.py
+++ image_adapt_serial.py
@@ -1,16 +1,22 @@
 from pylab import *
 import numpy as np
+import time
 
 # Update img directory to reflect github
-file_name = "input_small/114_ccd7.jpg"
+file_name = 'input_small/114_ccd7.jpg'
 original_image_rgb = imread(file_name)
 
 # Image is black and white so R=B=G
 IMG = array( original_image_rgb[:,:,0])
 
-# Set image dimensions
+# Get image data
 Lx = int32( IMG.shape[0])
 Ly = int32( IMG.shape[1])
+
+print "Processing %d x %d image" % (width, height)
+
+total_start_time = time.time()
+setup_start_time = time.time()
 
 # Allocate memory
 RAD = np.zeros((Lx, Ly), dtype=np.float64)
@@ -24,6 +30,9 @@ MaxRad = 30.0
 
 # Array to hold updated values
 ww = np.ones((Lx, Ly), dtype=np.float64)
+
+setup_stop_time = time.time()
+kernel_start_time = time.time()
 
 # Begin smoothing kernel
 for xx in range(Lx):
@@ -76,6 +85,15 @@ for xx in range(Lx):
                 sum += (IMG[xx+ii][yy+jj]*ww[ii+ss][jj+ss])
                 ksum += ww[ii+ss][jj+ss]
         OUT[xx][yy] = sum / ksum
-#---------------------------------------------------------------
-print "Processing %d x %d image" % (width, height)
 
+kernel_stop_time = time.time()
+total_stop_time = time.time()
+#---------------------------------------------------------------
+
+# Save the current image. NOT DONE
+imsave('input_small/114_ccd7{0}{1}.jpg'.format('_sharpen', '1'), OUT, cmap=cm.gray, vmin=0, vmax=1)
+
+# Print results & save
+print "Total Time: %f"      % (total_stop_time - total_start_time)
+print "Setup Time: %f"      % (setup_stop_time - setup_start_time)
+print "Kernel Time: %f"     % (kernel_stop_time - kernel_start_time)
