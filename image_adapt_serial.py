9ca3f1bd7db2fdcd38eea83bcb228dc3f2439fd8
Christopher K. Lee <chris.kelvin.lee@gmail.com>
2011-11-28 11:03:06 -0500

    commenting

--- image_adapt_serial.py
+++ image_adapt_serial.py
@@ -8,27 +8,32 @@ original_image_rgb = imread(file_name)
 # Image is black and white so R=B=G
 IMG = array( original_image_rgb[:,:,0])
 
-
+# Set image dimensions
 Lx = int32( IMG.shape[0])
 Ly = int32( IMG.shape[1])
 
+# Allocate memory
 RAD = np.zeros((Lx, Ly), dtype=np.float64)
 TOTAL = np.zeros((Lx, Ly), dtype=np.float64)
 NORM = np.zeros((Lx, Ly), dtype=np.float64)
 OUT = np.zeros((Lx, Ly), dtype=np.float64)
 
+# Parameters
 Threshold = 30.0
 MaxRad = 30.0
 
+# Array to hold updated values
 ww = np.ones((Lx, Ly), dtype=np.float64)
 
+# Begin smoothing kernel
 for xx in range(Lx):
     for yy in range(Ly):
-        qq = 0.0
-        sum = 0.0
-        ksum = 0.0
-        ss = qq
+        qq = 0.0        ##
+        sum = 0.0       ##
+        ksum = 0.0      ##
+        ss = qq         ##
 
+        # Continue until parameters
         while (sum < Threshold) and (qq < MaxRad):
             ss = qq
             sum = 0.0
@@ -40,27 +45,32 @@ for xx in range(Lx):
                     sum += IMG[xx + ii][yy + jj] * ww[ii + ss][jj + ss]
                     ksum +=(ww[ii + ss][jj + ss])
             qq += 1
-        
+            
+        # 
         RAD[xx][yy] = ss
         TOTAL[xx][yy] = sum
         
-
+        # Missing mm parameter??
         for ii in xrange( int(-ss), int(ss+1) ):
             for jj in xrange( int(-ss), int(ss+1) ):
                 NORM[xx+mm][yy+nn] += (ww[ii+ss][jj+ss])/ksum
 #---------------------------------------------------------------
 
+#
 for xx in range(Lx):
     for yy in range(Ly):
         IMG[xx][yy] /= NORM[xx][yy]
 
 #---------------------------------------------------------------
+
+#
 for xx in range(Lx):
     for yy in range(Ly):
         ss = RAD[xx][yy]
         sum = 0.0
         ksum = 0.0
 
+        #
         for ii in xrange( int(-ss), int(ss+1) ):
             for jj in xrange( int(-ss), int(ss+1) ):
                 sum += (IMG[xx+ii][yy+jj]*ww[ii+ss][jj+ss])
