863c9d26183290a4b0feccb655c4f85add1911ea
Danny Gibbs <dannygibbs@Endymion.local>
2011-11-29 20:35:21 -0500

    Updated serial code with three sets of for loops

--- image_adapt_serial.py
+++ image_adapt_serial.py
@@ -1,4 +1,5 @@
 from pylab import *
+import os
 import numpy as np
 import time
 
@@ -19,30 +20,24 @@ total_start_time = time.time()
 setup_start_time = time.time()
 
 # Allocate memory
-RAD = np.zeros((Lx, Ly), dtype=np.float64)      # ??? Matrix
-NORM = np.zeros((Lx, Ly), dtype=np.float64)     # Normalizing Matrix
-OUT = np.zeros((Lx, Ly), dtype=np.float64)      # Output Matrix
+RAD = np.zeros((Lx, Ly), dtype=np.float64)
+NORM = np.zeros((Lx, Ly), dtype=np.float64)
+OUT = np.zeros((Lx, Ly), dtype=np.float64)
 
-# Set parameters
-Threshold   = 10
-MaxRad      = 2
-
-# Array to hold updated values
-# This array can be used to implement the weight sums
-# for example gaussian, tophat, cone
-# currently set to one for general use.
-ww = 1.0#np.ones((Lx, Ly), dtype=np.float64)
+# Set Parameters
+Threshold = 10
+MaxRad = 2
 
 setup_stop_time = time.time()
 kernel_start_time = time.time()
 
-# Smoothing kernel
+# Begin smoothing kernel
 for xx in range(Lx):
     for yy in range(Ly):
-        qq = 0.0        ## size of box
-        sum = 0.0       ## value of the sum
-        ksum = 0.0      ## value of the kernal sum
-        ss = qq         ## size of the box around source pixel
+        qq = 0.0        # size of box
+        sum = 0.0       # value of the sum
+        ksum = 0.0      # value of the kernal sum
+        ss = qq         # size of the box around source pixel
         
         # Continue until parameters
         while (sum < Threshold) and (qq < MaxRad):
@@ -50,65 +45,65 @@ for xx in range(Lx):
             sum = 0.0
             ksum = 0.0
             
-            # Updated for loops for python
-            for ii in xrange( int(-ss), int(ss+1) ):
-                for jj in xrange( int(-ss), int(ss+1) ):
-                    #check for boundary condition
-                    #else skip to where qq = boundary
-                    if((xx + ss < Lx) and (yy + ss < Ly)):
-                        sum += IMG[xx + ii][yy + jj] * 1.0 #ww[ii + ss][jj + ss]
-                        ksum += 1.0 #(ww[ii + ss][jj + ss])
-                        NORM[xx+ii][yy+jj] += 1.0 / ksum #(ww[ii+ss][jj+ss])/ksum
-                    else:
-                        qq = MaxRad
+            #check for boundary condition
+            #else skip to where qq = boundary
+            if((xx + ss < Lx) and (yy + ss < Ly)):
+                # Updated for loops for python
+                for ii in xrange( int(-ss), int(ss+1) ):
+                    for jj in xrange( int(-ss), int(ss+1) ):
+                        sum += IMG[xx + ii][yy + jj]
+                        ksum += 1.0
+            else:
+                qq = MaxRad
             qq += 1
             
-        # Update value
+        # set the size of the radius for the determined box
         RAD[xx][yy] = ss
         
-        # Determine the normalization factor for each box
-        # for ii in xrange( int(-ss), int(ss+1) ):
-        #    for jj in xrange( int(-ss), int(ss+1) ):
-        #        if((xx + ss < Lx) and (yy + ss < Ly)):
-                    # CAN THIS LINE BE MOVED TO LINE 60 AFTER KSUM += 1.0?
-                    # THE MULTIPLE FOR LOOPS IS MAKING THE SERIAL VERSION RUN
-                    # ESPECIALLY SLOW
-                    # NORM[xx+ii][yy+jj] += 1.0 / ksum #(ww[ii+ss][jj+ss])/ksum
+        # Determine the normalization for each box
+        # Norm can't be determined from the above loop because it relies on the
+        # total ksum value, if placed above the incorrect ksum value will be
+        # divided.
+        for ii in xrange( int(-ss), int(ss+1) ):
+            for jj in xrange( int(-ss), int(ss+1) ):
+                if((xx + ss < Lx) and (yy + ss < Ly)):
+                    NORM[xx+ii][yy+jj] += 1.0 / ksum
 #---------------------------------------------------------------
 
-# Normalizing kernel
-##for xx in range(Lx):
-##    for yy in range(Ly):
+# Normalize the image
+for xx in range(Lx):
+    for yy in range(Ly):
         IMG[xx][yy] /= NORM[xx][yy]
-
+        
 #---------------------------------------------------------------
 
-# Output kernel
-##for xx in range(Lx):
-##    for yy in range(Ly):
+# Output file
+for xx in range(Lx):
+    for yy in range(Ly):
         ss = RAD[xx][yy]
         sum = 0.0
         ksum = 0.0
 
-        # IS THERE ANY REAASON WHY THIS LINE CANT BE COMBINED WITH THE ABOVE
-        # FOR LOOP
+        #
         for ii in xrange( int(-ss), int(ss+1) ):
             for jj in xrange( int(-ss), int(ss+1) ):
                 if((xx + ss < Lx) and (yy + ss < Ly)):
-                    sum += (IMG[xx+ii][yy+jj] * 1.0) #ww[ii+ss][jj+ss])
-                    ksum += 1.0 #ww[ii+ss][jj+ss]
+                    sum += IMG[xx+ii][yy+jj]
+                    ksum += 1.0
         #check for divide by zero
         if(ksum != 0):
             OUT[xx][yy] = sum / ksum
         else:
             OUT[xx][yy] = 0
 
+
+
 kernel_stop_time = time.time()
 total_stop_time = time.time()
 #---------------------------------------------------------------
 
 # Save the current image.
-imsave('{}_smoothed.png'.format(file_name), OUT, cmap=cm.gray, vmin=0, vmax=1)
+imsave('{}_serial_smoothed.png'.format(os.path.splitext(file_name)[0]), OUT, cmap=cm.gray, vmin=0, vmax=1)
 
 # Print results & save
 print "Total Time: %f"      % (total_stop_time - total_start_time)
